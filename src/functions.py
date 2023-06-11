267cf6a2d669c4c4f9fa84fa427a770dcdd2016c
Pavel Korekov <pkorekov@gmail.com>
2023-06-11 15:48:06 +0500

    add functions from program

---
+++ src/functions.py
@@ -0,0 +1,31 @@
+
+def read_json():
+    '''
+    читает файл json
+    :return: данные в виде списка словарей
+    '''
+    pass
+
+def sort_data(data):
+    '''
+    сортирует даннные по статусу операции
+    :param data: исходные даннные
+    :return: отсортированные данные
+    '''
+    pass
+
+def mask_card_number(card_number):
+    '''
+    Заменяет нужные цифры у номера карты на *
+    :param card_number: исходный номер карты
+    :return: строку со скрытыми данными
+    '''
+    pass
+
+def mask_amount_number(amount_number):
+    '''
+    Заменяет нужные цифры номера счета на *
+    :param amount_number: исходный номер счета
+    :return: строку со скрытыми данными
+    '''
+    pass
