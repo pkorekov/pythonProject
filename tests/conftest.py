31bb13042c41b038bbde896c49e04d9ac760e0e9
Pavel Korekov <pkorekov@gmail.com>
2023-06-12 12:50:56 +0500

    sort data tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -99,3 +99,56 @@ def expected_result_for_read_json_test():
 @pytest.fixture
 def path_to_test_json():
     return os.path.join(os.path.dirname(__file__), 'test_operations.json')
+
+@pytest.fixture
+def data_for_sort():
+    return [
+      {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+          "amount": "31957.58",
+          "currency": {
+            "name": "руб.",
+            "code": "RUB"
+          }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+      },
+      {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {
+          "amount": "67314.70",
+          "currency": {
+            "name": "руб.",
+            "code": "RUB"
+          }
+        },
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657"
+      }
+    ]
+@pytest.fixture
+def expected_result_for_sort():
+    return [
+      {
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+          "amount": "31957.58",
+          "currency": {
+            "name": "руб.",
+            "code": "RUB"
+          }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+      }]
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -7,3 +7,5 @@ from src.functions import read_json
 def test_read_json(path_to_test_json, expected_result_for_read_json_test):
     assert read_json(path_to_test_json) == expected_result_for_read_json_test
 
+def test_sort_data(data_for_sort, expected_result_for_sort):
+    assert sort_data(data_for_sort) == expected_result_for_sort
